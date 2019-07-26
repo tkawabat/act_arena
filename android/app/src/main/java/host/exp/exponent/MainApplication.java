dee3ed91f08ea837cf28f4a74fa0e7c772b0a21c
tkawabat <tosiki.kawabata@gmail.com>
2019-07-26 19:52:02 +0900

    sentry

--- android/app/src/main/java/host/exp/exponent/MainApplication.java
+++ android/app/src/main/java/host/exp/exponent/MainApplication.java
@@ -13,6 +13,7 @@ import okhttp3.OkHttpClient;
 
 // Needed for `react-native link`
 // import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 import jp.micin.react.skyway.SkyWayPackage;
 
 public class MainApplication extends ExpoApplication implements AppLoaderPackagesProviderInterface<ReactPackage> {
@@ -30,6 +31,7 @@ public class MainApplication extends ExpoApplication implements AppLoaderPackage
 
         // Needed for `react-native link`
         // new MainReactPackage(),
+            new RNSentryPackage(),
             new SkyWayPackage()
     );
   }
