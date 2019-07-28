7332fbc9c51e9ebd2b84f334c2d1b9f46e9be9c7
tkawabat <tosiki.kawabata@gmail.com>
2019-07-28 12:18:57 +0900

    fix android font load

--- App.tsx
+++ App.tsx
@@ -1,34 +1,21 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import {
-//     createSwitchNavigator,
-//     createBottomTabNavigator,
-//     createStackNavigator,
-//     createAppContainer,
-// } from 'react-navigation';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.tsx to start working on your app!</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
 import React, { Component } from 'react';
 import Navigator from './src/index';
 
 export default class App extends Component {
+    state = { fontLoaded: false };
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+                'Roboto': require('native-base/Fonts/Roboto.ttf'),
+                'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+                });
+
+        this.setState({fontLoaded: true});
+    }
+
   render() {
+      if (!this.state.fontLoaded) return null;
+
     return (
       <Navigator />
     );
