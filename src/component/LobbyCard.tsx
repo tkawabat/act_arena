e39d129a7c280ff55a8a0c798c69006149512191
tkawabat <tosiki.kawabata@gmail.com>
2019-08-10 22:46:29 +0900

    add LobbyCard

---
+++ src/component/LobbyCard.tsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import { Platform, StyleSheet, View, WebView } from 'react-native';
+import { Container, Header, Content, Text, Body, Button, Card, CardItem } from "native-base";
+
+import * as C from '../lib/Const';
+
+
+interface props {
+    title: string
+    , explain: string
+    , onPress: () => void
+}
+
+export default class LobbyIcon extends Component<props> {
+    render() {
+        return (
+            <Card>
+                <CardItem button onPress={this.props.onPress} header>
+                    <Text>{this.props.title}</Text>
+                </CardItem>
+                <CardItem button onPress={this.props.onPress}>
+                    <Body>
+                        <Text>
+                            {this.props.explain}
+                        </Text>
+                    </Body>
+                </CardItem>
+            </Card>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    button: {
+        height: 50,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 10,
+        paddingBottom: 10,
+    }
+    , title: {
+
+    }
+    , explain: {
+
+    }
+});
--- src/screen/LobbyScreen.tsx
+++ src/screen/LobbyScreen.tsx
@@ -7,17 +7,31 @@ import ScreenProps from './ScreenProps';
 import SkywayStore from '../model/SkywayStore';
 import Firebase from '../lib/Firebase';
 
+import LobbyCard from '../component/LobbyCard';
+
 @observer
 export default class LobbyScreen extends Component<ScreenProps> {
-    join = () => {
+    constructor(props) {
+        super(props);
+
+        this.joinArena = this.joinArena.bind(this);
+    }
+    
+    private joinArena() :void {
         const { navigation } = this.props;
         navigation.navigate('Push');
     }
-    modal = () => {
+    private modal() :void {
         const { navigation } = this.props
         navigation.navigate('Modal')
     }
+
     render() {
+        const cardItems = [
+            {
+                
+            }
+        ]
         return (
             <Container>
                 <Header>
@@ -27,7 +41,9 @@ export default class LobbyScreen extends Component<ScreenProps> {
                     </Body>
                     <Right />
                 </Header>
-                <View>
+                <LobbyCard title='アリーナ' explain='ランダムマッチで3分で声劇体験！' onPress={this.joinArena} />
+                <LobbyCard title='セリフバトル' explain='周りの投票で勝ち負けを決める！君の演技でいい感じに！' onPress={this.joinArena} />
+                {/* <View>
                     <Button small iconRight primary onPress={this.join}>
                         <Text>プッシュ表示</Text>
                     </Button>
@@ -36,21 +52,9 @@ export default class LobbyScreen extends Component<ScreenProps> {
                     <Button small iconRight transparent primary onPress={this.modal}>
                         <Text>モーダル表示</Text>
                     </Button>
-                </View>
-                <Text>{SkywayStore.state.toString()}</Text>
-                <Button small iconRight transparent onPress={() => SkywayStore.join()}>
-                    <Text>join</Text>
-                </Button>
-                <Button small iconRight transparent onPress={() => SkywayStore.leave()}>
-                    <Text>leave</Text>
-                </Button>
-                <Button small iconRight transparent onPress={() => SkywayStore.setLocalStreamStatus(true)}>
-                    <Text>open</Text>
-                </Button>
-                <Button small iconRight transparent onPress={() => SkywayStore.setLocalStreamStatus(false)}>
-                    <Text>close</Text>
-                </Button>
-                <Button small iconRight transparent onPress={() => Firebase.signup("random.matching@gmail.com", "cccccc")}>
+                </View> */}
+
+                {/* <Button small iconRight transparent onPress={() => Firebase.signup("random.matching@gmail.com", "cccccc")}>
                     <Text>signup</Text>
                 </Button>
                 <Button small iconRight transparent onPress={() => Firebase.login("random.matching@gmail.com", "cccccc")}>
@@ -64,7 +68,7 @@ export default class LobbyScreen extends Component<ScreenProps> {
                 </Button>
                 <Button small iconRight transparent onPress={() => Firebase.logout()}>
                     <Text>logout</Text>
-                </Button>
+                </Button> */}
             </Container>
         );
     }
