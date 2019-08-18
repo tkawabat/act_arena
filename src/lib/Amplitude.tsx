afe14e6b1f66a9afa06b5bee27509b3cc6d0ed79
tkawabat <tosiki.kawabata@gmail.com>
2019-08-18 23:01:14 +0900

    add Error log

--- src/lib/Amplitude.tsx
+++ src/lib/Amplitude.tsx
@@ -1,8 +1,16 @@
-import {Amplitude} from 'expo';
+import * as Amplitude from 'expo-analytics-amplitude';
 
 import Secret from './Secret';
 
 Amplitude.initialize(Secret.amplitude.apiKey);
 Amplitude.logEvent("init");
+
+export const Error = (event:string, error:any) => {
+    if (__DEV__) {
+        console.error('Error: '+event);
+        console.error(error);    
+    }
+    Amplitude.logEventWithProperties('Error: '+event, {error: error});
+}
 
 export default Amplitude;
