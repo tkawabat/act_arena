29c84f32cd2a59948d4b48421f3a35bdd9223560
tkawabat <tosiki.kawabata@gmail.com>
2019-09-08 11:22:14 +0900

    fix timer style

--- src/component/Timer.tsx
+++ src/component/Timer.tsx
@@ -1,20 +1,14 @@
 import Moment from 'moment';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-import { Container as View, Header, Left, Body, Right, Button, Icon, Title, H1 as Text, H2, H3} from 'native-base';
+import { View, Text } from 'native-base';
 import { observer } from 'mobx-react';
 
-import * as C from '../lib/Const';
 import ArenaStore from '../store/ArenaStore';
 
-interface props {
-    // url: string;
-    // start: string;
-    // end: string;
-}
 
 @observer
-export default class Timer extends Component<props> {
+export default class Timer extends Component {
     render() {
         return (
             <View style={styles.root}>
@@ -28,7 +22,7 @@ export default class Timer extends Component<props> {
 const styles = StyleSheet.create({
     root: {
         height: 72
-        , width: 72
+        , width: 60
         , textAlign: 'center'
         , justifyContent: 'center'
         //, color: '#FFF'
