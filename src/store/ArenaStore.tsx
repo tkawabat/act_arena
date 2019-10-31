bdc2cba83e88e850bc8cd507a04093d12a130b2a
tkawabat <tosiki.kawabata@gmail.com>
2019-11-01 02:15:42 +0900

    fix microphone bug

--- src/store/ArenaStore.tsx
+++ src/store/ArenaStore.tsx
@@ -179,15 +179,17 @@ class ArenaStore {
         this.overlayMessage = data.message;
         this.characters = data.characters;
 
-        for (const character of this.characters) {
-            if (character.user !== UserStore.id) continue;
+        if (this.arenaState !== C.ArenaState.WAIT) {
+            for (const character of this.characters) {
+                if (character.user !== UserStore.id) continue;
 
-            Amplitude.info('ArenaBeActor', null);
-            if (SkywayStore.speakState === C.SpeakState.DISABLED) {
-                SkywayStore.setSpeakState(C.SpeakState.MUTE);
-                SkywayStore.toggleMicrophone();
+                Amplitude.info('ArenaBeActor', null);
+                if (SkywayStore.speakState === C.SpeakState.DISABLED) {
+                    SkywayStore.setSpeakState(C.SpeakState.MUTE);
+                    SkywayStore.toggleMicrophone();
+                }
+                break;
             }
-            break;
         }
 
         clearInterval(this.tickTimeout);
