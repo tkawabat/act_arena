b1755f6b5731dd28c054f18ce5d288d9081f88d5
tkawabat <tosiki.kawabata@gmail.com>
2019-11-23 15:31:04 +0900

    add storyLoader

---
+++ storybook/storyLoader.tsx
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/component/admin/index.stories');
+  require('../src/component/atom/index.stories');
+  require('../src/component/index.stories');
+}
+
+const stories = [
+	'../src/component/index.stories'
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
