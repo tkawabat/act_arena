669fe4aeef0d339ca08c681b8613c37faa50e873
tkawabat <tosiki.kawabata@gmail.com>
2020-01-15 22:56:29 +0900

    android build success

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp30ebfdb703d64aa89d572e3b49377404://192.168.11.8:19000";
+
+}
