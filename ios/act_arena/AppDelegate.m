aa5dd451dc98b1cc6f2275fd0de3afcbabc1487c
tkawabat <tosiki.kawabata@gmail.com>
2020-02-20 00:43:51 +0900

    fix ios release build

--- ios/act_arena/AppDelegate.m
+++ ios/act_arena/AppDelegate.m
@@ -6,6 +6,7 @@
 
 @implementation AppDelegate
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 //    if ([FIRApp defaultApp] == nil) {
