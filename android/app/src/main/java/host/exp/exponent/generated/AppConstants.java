cb4f125dee1a42ddaeec1687ce3f796c80bc093a
tkawabat <tosiki.kawabata@gmail.com>
2020-02-29 11:43:31 +0900

    upgrade android version

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -46,10 +46,12 @@ public class AppConstants {
         
         
         
+        
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@random.matching/act_arena", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40random.matching%2Fact_arena%2F1.0.0%2F9e79cd1f6f3bfe80c2e94954c6c27028-36.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40random.matching%2Fact_arena%2F1.0.0%2Fc43afbbf9812cac1478ca573062b87fa-36.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
