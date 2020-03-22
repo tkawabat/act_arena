8706ad5f6ca7c80693017254a8d8bd3d40a15e79
tkawabat <tosiki.kawabata@gmail.com>
2020-03-22 22:41:30 +0900

    add disabled to LobbyCardMatching

--- src/component/l2/LobbyCardMatching.tsx
+++ src/component/l2/LobbyCardMatching.tsx
@@ -7,9 +7,11 @@ import * as C from '../../lib/Const';
 import * as BasicStyle from '../../lib/BasicStyle';
 
 import MatchingStore from '../../store/MatchingStore';
+import LobbyStore from '../../store/LobbyStore';
 
 import TextBadge from '../l1/TextBadge';
 import LobbyCardBase from '../l1/LobbyCardBase';
+
 
 interface props {
 }
@@ -19,24 +21,27 @@ export default class LobbyCardMatching extends Component<props> {
     
     render() {
         let animation = null;
-            if (MatchingStore.isMatching) {
-                animation = (
+        if (MatchingStore.isMatching) {
+            animation = (
                 <Animation animation={'jello'} duration={4000} iterationCount={'infinite'}>
                     <TextBadge text={'マッチング中'} />
                 </Animation>
-                );
-            } else {
-                animation = (
+            );
+        } else {
+            animation = (
                 <Animation animation={'fadeIn'} easing={'ease-in-out'} duration={4000} iterationCount={'infinite'}>
-                        <PressText>タップしてマッチング</PressText>
+                    <PressText>タップしてマッチング</PressText>
                 </Animation>
-                );
-            }
+            );
+        }
+        
+        const disabled = LobbyStore.actTheaterId !== undefined;
 
         return (
             <LobbyCardBase
                 title={'サシ劇マッチング'}
                 onPress={MatchingStore.toggle.bind(this)}
+                disabled={disabled}
             >
                 <Body>
                     <ExplainText>サクッと１本劇しよう！ 読み時間合わせて60分。</ExplainText>
